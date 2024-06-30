ba991d9bd3127748f071fcce8376beaa798c662a
aaron <aaronkornish.ak@gmail.com>
2024-06-30 00:14:58 -0700

    Created a navbar with links to social media accounts and navlinks to other pages of portfolio.

--- my-portfolio/src/components/header.jsx
+++ my-portfolio/src/components/header.jsx
@@ -1,8 +1,40 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
-function Header () {
-    return (
-        <p>header</p>
-    )
-}
+const Header = () => {
+	return (
+		<>
+			<header>
+				<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+					<Container>
+						<LinkContainer to="/">
+							<Navbar.Brand>NavBar</Navbar.Brand>
+						</LinkContainer>
+						<Nav>
+							<LinkContainer to="/about">
+								<Nav.Link>about</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to="/portfolio">
+								<Nav.Link>portfolio</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to="/contact">
+								<Nav.Link>contact</Nav.Link>
+							</LinkContainer>
 
-export default Header
+							<Link className="p-2" to="https://x.com/grunge_phase">
+								<a className="fa fa-twitter"></a>
+							</Link>
+
+							<Link className="p-2" to="https://www.linkedin.com/in/aaron-kornish">
+								<a className="fa fa-linkedin"></a>
+							</Link>
+						</Nav>
+					</Container>
+				</Navbar>
+			</header>
+		</>
+	);
+};
+
+export default Header;
