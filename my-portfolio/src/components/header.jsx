a8dd19343de0929698f308eb301a77b7d2df7f94
aaron <aaronkornish.ak@gmail.com>
2024-06-30 10:39:40 -0700

    Added hamburger menu when adjusting the size of the page.

--- my-portfolio/src/components/header.jsx
+++ my-portfolio/src/components/header.jsx
@@ -6,30 +6,39 @@ const Header = () => {
 	return (
 		<>
 			<header>
-				<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+				<Navbar bg="dark" variant="dark" expand="md">
 					<Container>
 						<LinkContainer to="/">
-							<Navbar.Brand>NavBar</Navbar.Brand>
+							<Navbar.Brand>NexusGrove</Navbar.Brand>
 						</LinkContainer>
-						<Nav>
-							<LinkContainer to="/about">
-								<Nav.Link>about</Nav.Link>
-							</LinkContainer>
-							<LinkContainer to="/portfolio">
-								<Nav.Link>portfolio</Nav.Link>
-							</LinkContainer>
-							<LinkContainer to="/contact">
-								<Nav.Link>contact</Nav.Link>
-							</LinkContainer>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse className="justify-content-end">
+							<Nav>
+								<LinkContainer to="/about">
+									<Nav.Link>about</Nav.Link>
+								</LinkContainer>
+								<LinkContainer to="/portfolio">
+									<Nav.Link>portfolio</Nav.Link>
+								</LinkContainer>
+								<LinkContainer to="/contact">
+									<Nav.Link>contact</Nav.Link>
+								</LinkContainer>
 
-							<Link className="p-2" to="https://x.com/grunge_phase">
-								<a className="fa fa-twitter"></a>
-							</Link>
+								<Link
+									className="p-2"
+									to="https://x.com/grunge_phase"
+								>
+									<a className="fa fa-twitter"></a>
+								</Link>
 
-							<Link className="p-2" to="https://www.linkedin.com/in/aaron-kornish">
-								<a className="fa fa-linkedin"></a>
-							</Link>
-						</Nav>
+								<Link
+									className="p-2"
+									to="https://www.linkedin.com/in/aaron-kornish"
+								>
+									<a className="fa fa-linkedin"></a>
+								</Link>
+							</Nav>
+						</Navbar.Collapse>
 					</Container>
 				</Navbar>
 			</header>
