18877ce593d2e9aeeaad6b004e7b6772ff2578b8
aaron <aaronkornish.ak@gmail.com>
2024-06-30 11:10:08 -0700

    Created backsplash component with image.

---
+++ my-portfolio/src/components/backsplash.jsx
@@ -0,0 +1,11 @@
+import backsplashImg from "../assets/neon_grid_green.jpg"
+
+const Backsplash = () => {
+    return (
+        <>
+            <img className="w-100" src={backsplashImg} />
+        </>
+    )
+}
+
+export default Backsplash
