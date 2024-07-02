f8fd738b3e7e26997722ad2576ee3d188a5c31dc
aaron <aaronkornish.ak@gmail.com>
2024-07-01 19:14:04 -0700

    Imported PortfolioCard and cardInfo to mainContent component. Mapped through cardInfo and listed them with PortfolioCard components.

--- my-portfolio/src/components/mainContent.jsx
+++ my-portfolio/src/components/mainContent.jsx
@@ -1,4 +1,6 @@
+import PortfolioCard from "./portfolioCard"
 import {Col, Row, Container} from "react-bootstrap"
+import cardInfo from "../cardInfo.js"
 
 const MainContent = () => {
     return (
@@ -9,6 +11,17 @@ const MainContent = () => {
                     <Col className="text-center" xs={4}><p>Some of my work</p></Col>
                     <Col xs={4}><hr/></Col>
                 </Row>
+                <Row>
+                    {cardInfo.map((singleCard) => (
+                        <Col key={singleCard.id}>
+                        <PortfolioCard 
+                        text={singleCard.text} 
+                        title={singleCard.title}
+                        />
+                        </Col>
+                        ))}
+                </Row>
+                
             </Container>
         </>
     )
