e838de4bdbe70a55774d143a76469cc54cda98d0
aaron <aaronkornish.ak@gmail.com>
2024-07-02 19:24:21 -0700

    Created all routes children routes in the parent route element App.

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import {
 	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
 	RouterProvider,
 } from "react-router-dom";
+import MainScreen from "./screens/mainScreen.jsx";
+import About from "./screens/about.jsx";
+import Portfolio from "./screens/portfolio.jsx";
+import Contact from "./screens/contact.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>
-  }
-])
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App/>}>
+			<Route path="/" element={<MainScreen/>} />
+			<Route path="/about" element={<About/>} />
+			<Route path="/portfolio" element={<Portfolio/>} />
+			<Route path="/contact" element={<Contact/>} />
+		</Route>
+	)
+)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
