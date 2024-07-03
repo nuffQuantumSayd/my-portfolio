062e88c5109e8658e6e948c2d5a45ed753ad6896
aaron <aaronkornish.ak@gmail.com>
2024-07-02 20:31:25 -0700

    Created AboutBody component.

---
+++ src/components/aboutBody.jsx
@@ -0,0 +1,30 @@
+import { Container } from "react-bootstrap";
+
+function AboutBody() {
+    return ( 
+        <>
+        <div className="about-body text-center pt-5 pb-5">
+				<Container>
+					<p className="p-5 m-0">
+						Passionate and motivated computer programming student
+						seeking opportunities to apply and enhance my technical
+						skills. Currently pursuing a BS Software Engineering at
+						Western Governors University . Skilled in Java and .NET,
+						I thrive in challenging environments that allow me to
+						leverage my problem-solving abilities. As an avid
+						learner, I am constantly seeking new knowledge and
+						staying up-to-date with the latest industry trends.
+						Through my academic projects, I have gained practical
+						experience in front end web development and databases. I
+						am eager to contribute to innovative projects and
+						embrace new technologies that drive advancements in the
+						field of computer programming. Connect with me to
+						explore potential collaborations and opportunities!
+					</p>
+				</Container>
+			</div>
+        </>
+     );
+}
+
+export default AboutBody;
