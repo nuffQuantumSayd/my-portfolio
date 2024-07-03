1de0addbdfcdf5de6b0ede0e706f1408fc1d52c9
aaron <aaronkornish.ak@gmail.com>
2024-07-02 20:32:37 -0700

    Imported AboutIntro, AboutBody, and AboutPic components to About component.

--- src/screens/about.jsx
+++ src/screens/about.jsx
@@ -1,9 +1,17 @@
-const About = () => {
-    return(
-        <>
-        about
-        </>
-    )
-}
 
-export default About
+import "../css/about.css";
+import AboutIntro from "../components/aboutIntro";
+import AboutBody from "../components/aboutBody";
+import AboutPic from "../components/aboutPic";
+
+const About = () => {
+	return (
+		<>
+			<AboutIntro />
+			<AboutBody />
+            <AboutPic />
+		</>
+	);
+};
+
+export default About;
