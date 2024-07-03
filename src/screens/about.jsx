3d0a738ea75d7a758c427ec48aad252574e115e9
aaron <aaronkornish.ak@gmail.com>
2024-07-02 19:21:03 -0700

    Created the different pages in the project.

---
+++ src/screens/about.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return(
+        <>
+        about
+        </>
+    )
+}
+
+export default About
---
+++ src/screens/mainScreen.jsx
@@ -0,0 +1,13 @@
+import Backsplash from "../components/backsplash"
+import MainContent from "../components/mainContent"
+
+const MainScreen = () => {
+    return(
+        <>
+            <Backsplash/>
+            <MainContent/>
+        </>
+    )
+}
+
+export default MainScreen
