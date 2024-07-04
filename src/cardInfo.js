795de6e9d3d593bbc4216acea1154513b7f43322
aaron <aaronkornish.ak@gmail.com>
2024-07-04 16:15:25 -0700

    Updated the image path in cardInfo file

--- src/cardInfo.js
+++ src/cardInfo.js
@@ -2,19 +2,19 @@
 const cardInfo = [
         {
             "id": 0,
-            "image": "image1",
+            "image": "public/images/book-bazaar_inPixio_tk_vid.jpg",
             "title": "Book Bazzar",
             "text": "BookBazaar is an e-commerce platform dedicated to providing an online shopping experience for books. Explore our books, ranging from timeless classics to contemporary bestsellers, and embark on a journey where every click opens a new chapter."
         },
         {
             "id": 1,
-            "image": "/images/weather-app-pic.png",
+            "image": "public/images/weather-app-pic_inPixio_tk_vid.jpg",
             "title": "Weather App",
             "text": "WeatherNow is a simple and intuitive Android app that allows users to search for weather conditions by city and view both the current weather details and a five-day forecast. With a user-friendly interface and real-time weather data, users can stay informed about the weather in their desired locations."
         },
         {
             "id": 2,
-            "image": "image1",
+            "image": "public/images/Animal_1_inPixio_tk_vid.png",
             "title": "Pound",
             "text": "Welcome to the Mock Rescue Dog Shelter Application README. This application is a fictional web-based platform designed to help manage a rescue dog shelter. It allows users to view available dogs for adoption, submit adoption applications, and for administrators, provides CRUD (Create, Read, Update, Delete) functionality to manage dog listings, applications, and user accounts."
         }
